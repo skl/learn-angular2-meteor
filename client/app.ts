bcf5bfadbe7b28789c7553e3a673d553f2beac87
Stephen Lang <stephen@mediabowl.co.uk>
2015-12-22 10:47:10 +0000

    0. Bootstrapping

---
+++ client/app.ts
@@ -0,0 +1,15 @@
+/// <reference path="../typings/angular2-meteor.d.ts" />
+
+import {Component, View} from 'angular2/core';
+
+import {bootstrap} from 'angular2/bootstrap';
+
+@Component({
+    selector: 'app'
+})
+@View({
+    templateUrl: 'client/app.html'
+})
+class Socially { }
+
+bootstrap(Socially);

6d98c379d7e028827ad80a5270a8727115b5a195
Stephen Lang <stephen@mediabowl.co.uk>
2015-12-22 13:11:23 +0000

    3. 3-Way data binding

--- client/app.ts
+++ client/app.ts
@@ -1,10 +1,12 @@
 /// <reference path="../typings/angular2-meteor.d.ts" />
 
-import {Component, View} from 'angular2/core';
+import {Component, View, NgZone} from 'angular2/core';
 
 import {NgFor} from 'angular2/common';
 
-import {bootstrap} from 'angular2/bootstrap';
+import {bootstrap} from 'angular2-meteor';
+
+import {Parties} from 'collections/parties';
 
 @Component({
     selector: 'app'
@@ -14,26 +16,10 @@ import {bootstrap} from 'angular2/bootstrap';
     directives: [NgFor]
 })
 class Socially {
-    parties: Array<Object>;
+    parties: Mongo.Cursor<Object>;
 
     constructor () {
-        this.parties = [
-            {
-                'name': 'Dubstep-Free Zone',
-                'description': 'Can we please just for an evening not listen to dubstep.',
-                'location': 'Palo Alto'
-            },
-            {
-                'name': 'All dubstep all the time',
-                'description': 'Get it on!',
-                'location': 'Palo Alto'
-            },
-            {
-                'name': 'Savage lounging',
-                'description': 'Leisure suit required. And only fiercest manners.',
-                'location': 'San Fransisco'
-            }
-        ];
+        this.parties = Parties.find();
     }
 }
 

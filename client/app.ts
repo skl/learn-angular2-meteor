48fe1aabebde0f4510428b4ede1e0b7ca6a04ef4
Stephen Lang <stephen@mediabowl.co.uk>
2015-12-22 11:22:59 +0000

    2. Dynamic Template

--- client/app.ts
+++ client/app.ts
@@ -2,14 +2,39 @@
 
 import {Component, View} from 'angular2/core';
 
+import {NgFor} from 'angular2/common';
+
 import {bootstrap} from 'angular2/bootstrap';
 
 @Component({
     selector: 'app'
 })
 @View({
-    templateUrl: 'client/app.html'
+    templateUrl: 'client/app.html',
+    directives: [NgFor]
 })
-class Socially { }
+class Socially {
+    parties: Array<Object>;
+
+    constructor () {
+        this.parties = [
+            {
+                'name': 'Dubstep-Free Zone',
+                'description': 'Can we please just for an evening not listen to dubstep.',
+                'location': 'Palo Alto'
+            },
+            {
+                'name': 'All dubstep all the time',
+                'description': 'Get it on!',
+                'location': 'Palo Alto'
+            },
+            {
+                'name': 'Savage lounging',
+                'description': 'Leisure suit required. And only fiercest manners.',
+                'location': 'San Fransisco'
+            }
+        ];
+    }
+}
 
 bootstrap(Socially);

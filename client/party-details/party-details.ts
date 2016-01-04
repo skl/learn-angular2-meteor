7d718ddcf06fb3d16c40b86b2b831e704273ce2d
Stephen Lang <stephen@mediabowl.co.uk>
2016-01-04 14:21:43 +0000

    6. Bind one object

--- client/party-details/party-details.ts
+++ client/party-details/party-details.ts
@@ -1,14 +1,17 @@
 /// <reference path="../../typings/angular2-meteor.d.ts" />
 
 import {Component, View} from 'angular2/core';
+import {FORM_DIRECTIVES} from 'angular2/common';
 import {RouteParams} from 'angular2/router';
 import {Parties} from 'collections/parties';
+import {RouterLink} from 'angular2/router';
 
 @Component({
     selector: 'party-details'
 })
 @View({
-    templateUrl: '/client/party-details/party-details.html'
+    templateUrl: '/client/party-details/party-details.html',
+    directives: [RouterLink, FORM_DIRECTIVES]
 })
 export class PartyDetails {
     party: Object;
@@ -16,5 +19,15 @@ export class PartyDetails {
     constructor(params: RouteParams) {
         var partyId = params.get('partyId');
         this.party = Parties.findOne(partyId);
+    }
+
+    saveParty(party) {
+        Parties.update(party._id, {
+            $set: {
+                name: party.name,
+                description: party.description,
+                location: party.location
+            }
+        });
     }
 }

4ba4ec595acd0ddc4a471c0c1210945e72564201
Stephen Lang <stephen@mediabowl.co.uk>
2016-01-04 11:36:38 +0000

    5. Routing & Multiple Views

--- client/app.ts
+++ client/app.ts
@@ -1,31 +1,22 @@
 /// <reference path="../typings/angular2-meteor.d.ts" />
 
-import {Component, View, NgZone} from 'angular2/core';
-
-import {NgFor} from 'angular2/common';
-
+import {Component, View, NgZone, provide} from 'angular2/core';
 import {bootstrap} from 'angular2-meteor';
-
-import {Parties} from 'collections/parties';
-import {PartiesForm} from "client/parties-form/parties-form";
+import {ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig, APP_BASE_HREF} from 'angular2/router';
+import {PartiesList} from 'client/parties-list/parties-list';
+import {PartyDetails} from 'client/party-details/party-details';
 
 @Component({
     selector: 'app'
 })
 @View({
-    templateUrl: 'client/app.html',
-    directives: [NgFor, PartiesForm]
+    template: '<router-outlet></router-outlet>',
+    directives: [ROUTER_DIRECTIVES]
 })
-class Socially {
-    parties: Mongo.Cursor<Object>;
+@RouteConfig([
+    { path: '/', as: 'PartiesList', component: PartiesList },
+    { path: '/party/:partyId', as: 'PartyDetails', component: PartyDetails }
+])
+class Socially { }
 
-    constructor () {
-        this.parties = Parties.find();
-    }
-
-    removeParty(party) {
-        Parties.remove(party._id);
-    }
-}
-
-bootstrap(Socially);
+bootstrap(Socially, [ROUTER_PROVIDERS, provide(APP_BASE_HREF, { useValue: '/' })]);
---
+++ client/parties-list/parties-list.ts
@@ -0,0 +1,26 @@
+/// <reference path="../../typings/angular2-meteor.d.ts" />
+
+import {Component, View} from 'angular2/core';
+import {NgFor} from 'angular2/common';
+import {Parties} from 'collections/parties';
+import {PartiesForm} from 'client/parties-form/parties-form';
+import {RouterLink} from 'angular2/router';
+
+@Component({
+    selector: 'app'
+})
+@View({
+    templateUrl: 'client/parties-list/parties-list.html',
+    directives: [NgFor, PartiesForm, RouterLink]
+})
+export class PartiesList{
+    parties: Mongo.Cursor<Object>;
+
+    constructor () {
+        this.parties = Parties.find();
+    }
+
+    removeParty(party) {
+        Parties.remove(party._id);
+    }
+}
